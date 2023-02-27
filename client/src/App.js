1a61581b76b2cc68c55655639bcb75cd9ac6c70e
Pasha <zenoken4ik@gmail.com>
2023-02-27 17:12:44 +0200

    created react app

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+import { useEffect } from 'react';
+
+function App() {
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
-
+app.use(cors())
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }));
